5a8cb55ff3293c5d50613a6ad9bd06972c7a9812
Wouter Kleijn <37488847+wouterkleijn@users.noreply.github.com>
2018-04-11 01:51:07 +0200

    Create homework 1.sh

---
+++ commandline/week1/homework 1.sh
@@ -0,0 +1,16 @@
+cd homework
+chflags hidden hidden.txt
+ls
+mkdir -p these/folders/are/just/for/fun
+echo "test" > fun
+echo "another test" >> fun
+cd these/folders/are/just/for/fun
+echo "test" > fun
+echo "another test" >> fun
+wc -c fun
+     18 fun
+cat fun
+test
+another test
+echo "test" > fun
+echo -n " another test" >> fun
